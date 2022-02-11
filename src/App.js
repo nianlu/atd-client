e4f9547738749007ae3a607ea15592cf2aab3f69
nianlu <nianlushu@gmail.com>
2022-02-10 21:55:10 -0500

    login google

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import GoogleLogin from 'react-google-login';
+
+const responseGoogle = (response) => {
+  console.log(response);
+}
+
 function App() {
+
+  const [ userProfile, setUserProfile ] = useState()
+
+  console.log('user', userProfile)
+
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello {userProfile?.name}</p>
+        <GoogleLogin
+          clientId="306964030038-nmaje4copbq90ukj482qf5523o7nu57i.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={r => setUserProfile(r.profileObj)}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+          isSignedIn={true}
+        />
       </header>
     </div>
   );
